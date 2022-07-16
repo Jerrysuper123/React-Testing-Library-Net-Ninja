a2a47339ff999e44bd062dc8619267ef293c6c63
Jerry Chen <jerryq.chen@gmail.com>
2022-07-16 16:05:31 +0800

    completed mock API

--- src/components/AddInput/__test__/AddInput.test.js
+++ src/components/AddInput/__test__/AddInput.test.js
@@ -1,59 +1,57 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import AddInput from "../AddInput"
+import { render, screen, fireEvent } from "@testing-library/react";
+import AddInput from "../AddInput";
 
+//mock function with empty function
 const mockedSetTodo = jest.fn();
 
 describe("AddInput", () => {
-    it('should render input element', () => {
-        render(
-            <AddInput 
-                todos={[]}
-                setTodos={mockedSetTodo}
-            />
-        );
-        const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-        expect(inputElement).toBeInTheDocument();
+  it("should render input element", () => {
+    render(
+      <AddInput
+        // set toDos with empty array
+        todos={[]}
+        setTodos={mockedSetTodo}
+      />
+    );
+    //grab placeholder
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it("should be able to type into input", () => {
+    render(<AddInput todos={[]} setTodos={mockedSetTodo} />);
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    fireEvent.click(inputElement);
+    //type into input
+    fireEvent.change(inputElement, {
+      target: { value: "Go Grocery Shopping" },
     });
-    
-    it('should be able to type into input', () => {
-        render(
-            <AddInput 
-                todos={[]}
-                setTodos={mockedSetTodo}
-            />
-        );
-        const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-        fireEvent.click(inputElement)
-        fireEvent.change(inputElement, { target: { value: "Go Grocery Shopping" } })
-        expect(inputElement.value).toBe("Go Grocery Shopping");
+    expect(inputElement.value).toBe("Go Grocery Shopping");
+  });
+
+  it("should be able to add into input", () => {
+    render(<AddInput todos={[]} setTodos={mockedSetTodo} />);
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    fireEvent.click(inputElement);
+    fireEvent.change(inputElement, {
+      target: { value: "Go Grocery Shopping" },
     });
-    
-    it('should be able to type into input', () => {
-        render(
-            <AddInput 
-                todos={[]}
-                setTodos={mockedSetTodo}
-            />
-        );
-        const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-        fireEvent.click(inputElement)
-        fireEvent.change(inputElement, { target: { value: "Go Grocery Shopping" } });
-        const buttonElement = screen.getByRole("button", { name: /Add/i});
-        fireEvent.click(buttonElement)
-        expect(mockedSetTodo).toBeCalled()
+    //grab the button
+    const buttonElement = screen.getByRole("button", { name: /Add/i });
+    fireEvent.click(buttonElement);
+    //expect mock function to be called
+    expect(mockedSetTodo).toBeCalled();
+  });
+
+  it("should have empty input when add button is cliked", () => {
+    render(<AddInput todos={[]} setTodos={mockedSetTodo} />);
+    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+    fireEvent.change(inputElement, {
+      target: { value: "Go Grocery Shopping" },
     });
-    
-    it('should have empty input when add button is cliked', () => {
-        render(
-            <AddInput 
-                todos={[]}
-                setTodos={mockedSetTodo}
-            />
-        );
-        const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-        fireEvent.change(inputElement, { target: { value: "Go Grocery Shopping" } });
-        const buttonElement = screen.getByRole("button", { name: /Add/i});
-        fireEvent.click(buttonElement)
-        expect(inputElement.value).toBe("")
-    });
-})
+    const buttonElement = screen.getByRole("button", { name: /Add/i });
+    fireEvent.click(buttonElement);
+    //once added, expect input value to be empty
+    expect(inputElement.value).toBe("");
+  });
+});
--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -1,48 +1,46 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import FollowersList from "../FollowersList";
 
+// mock react router because the component uses <LinK>
 const MockFollowersList = () => {
-    return (
-        <BrowserRouter>
-            <FollowersList />
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <FollowersList />
+    </BrowserRouter>
+  );
+};
 
 describe("FollowersList", () => {
+  //mock the API
+  //   beforeEach(() => {
+  //     // console.log("RUNS BEFORE EACH TEST")
+  //     // jest.mock("../../../__mocks__/axios");
+  //   });
 
-    beforeEach(() => {
-        // console.log("RUNS BEFORE EACH TEST")
-        jest.mock("../../../__mocks__/axios")
-    })
+  // beforeAll(() => {
+  //     console.log("RUNS ONCE BEFORE ALL TESTS")
+  // })
 
-    // beforeAll(() => {
-    //     console.log("RUNS ONCE BEFORE ALL TESTS")
-    // })
+  // afterEach(() => {
+  //     console.log("RUNS AFTER EACH TEST")
+  // })
 
-    // afterEach(() => {
-    //     console.log("RUNS AFTER EACH TEST")
-    // })
+  // afterAll(() => {
+  //     console.log("RUNS ONCE AFTER ALL TESTS")
+  // })
 
-    // afterAll(() => {
-    //     console.log("RUNS ONCE AFTER ALL TESTS")
-    // })
+  it("should fetch and render input element", async () => {
+    //   after setting up mock api, below when run axios will take value from mock
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByTestId(`follower-item-0`);
+    expect(followerDivElement).toBeInTheDocument();
+  });
 
-    it('should fetch and render input element', async () => {
-        render(
-            <MockFollowersList />
-        );
-        const followerDivElement = await screen.findByTestId(`follower-item-0`)
-        expect(followerDivElement).toBeInTheDocument();
-    });
-    
-    it('should fetch and render input element', async () => {
-        render(
-            <MockFollowersList />
-        );
-    
-        const followerDivElement = await screen.findByTestId(`follower-item-0`)
-        expect(followerDivElement).toBeInTheDocument();
-    });
-})
+  //   it("should fetch and render input element", async () => {
+  //     render(<MockFollowersList />);
+
+  //     const followerDivElement = await screen.findByTestId(`follower-item-0`);
+  //     expect(followerDivElement).toBeInTheDocument();
+  //   });
+});
